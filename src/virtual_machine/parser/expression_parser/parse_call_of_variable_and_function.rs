fb0a5b803b825d3b7a49929f82caadac9d37e956
Shun <s.tsuchiya.business@gmail.com>
2024-11-18 18:08:14 +0900

    引数が一つの関数のパースが可能か確認するテストを追加しました

--- src/virtual_machine/parser/expression_parser/parse_call_of_variable_and_function.rs
+++ src/virtual_machine/parser/expression_parser/parse_call_of_variable_and_function.rs
@@ -62,6 +62,7 @@ pub fn parse_identifier_or_call(parser: &mut Parser) -> Result<ExpressionNode, P
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::virtual_machine::ast::{LiteralNode, LiteralValue};
     use crate::virtual_machine::parser::Parser;
     use crate::virtual_machine::token::{token_type::TokenType, Token};
 
@@ -70,6 +71,8 @@ mod tests {
         tokens_with_eof.push(Token::new(1, 1, TokenType::Eof)); // EOFを追加
         Parser::new(tokens_with_eof)
     }
+
+    // 正常系テスト
 
     /// 引数が存在しない関数のパースが可能か確認するテスト
     /// f();
@@ -101,6 +104,41 @@ mod tests {
             _ => panic!("ExpectedNode::CallOfFunction"),
         };
 
+        assert_eq!(call_of_function, expected);
+    }
+
+    /// 引数が存在する関数のパースが可能か確認するテスト
+    /// f(0);
+    #[test]
+    fn parse_function_with_an_argument() {
+        // 生成されるAST Node
+        let expected: Box<FunctionCallNode> = Box::new(FunctionCallNode {
+            name: "f".to_string(),
+            arguments: vec![ExpressionNode::Literal(Box::new(LiteralNode {
+                value: LiteralValue::Integer(0),
+            }))],
+        });
+
+        // テストする関数の入力である、Token列, Parserの生成
+        // f();
+        let tokens: Vec<Token> = vec![
+            Token::new(1, 1, TokenType::Identifier("f".to_string())),
+            Token::new(1, 2, TokenType::LeftParen),
+            Token::new(1, 3, TokenType::IntegerLiteral(0)),
+            Token::new(1, 3, TokenType::RightParen),
+            Token::new(1, 4, TokenType::Semicolon),
+        ];
+        let mut parser: Parser = create_parser_with_tokens(tokens);
+
+        // テストしたい関数の出力 (エラーが出ていないことを確認)
+        let result: Result<ExpressionNode, ParserError> = parse_identifier_or_call(&mut parser);
+        assert!(result.is_ok());
+
+        // テストしたい関数の出力と期待値を比較
+        let call_of_function: Box<FunctionCallNode> = match result.clone().unwrap() {
+            ExpressionNode::CallOfFunction(call_of_function) => call_of_function,
+            _ => panic!("ExpectedNode::CallOfFunction"),
+        };
         assert_eq!(call_of_function, expected);
     }
 }
