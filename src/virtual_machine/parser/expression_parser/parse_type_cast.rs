50a0776995a781ebe923573999522ec07811410a
Shun <s.tsuchiya.business@gmail.com>
2024-11-22 23:41:11 +0900

    add type cast test

--- src/virtual_machine/parser/expression_parser/parse_type_cast.rs
+++ src/virtual_machine/parser/expression_parser/parse_type_cast.rs
@@ -34,3 +34,57 @@ pub fn parse_type_cast(
         expression: Box::new(expr),
     })))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::virtual_machine::ast::{LiteralNode, LiteralValue, Type, TypeCastNode};
+    use crate::virtual_machine::parser::expression_parser::parse_type_cast::parse_type_cast;
+    use crate::virtual_machine::parser::parser_error::ParserError;
+    use crate::virtual_machine::parser::{Parser, TokenType};
+    use crate::virtual_machine::token::token_type::TokenType::IntegerLiteral;
+    use crate::virtual_machine::token::Token;
+    use shot::virtual_machine::ast::ExpressionNode;
+
+    fn create_parser_with_tokens(tokens: Vec<Token>) -> Parser {
+        let mut tokens_with_eof = tokens.clone();
+        tokens_with_eof.push(Token::new(1, 1, TokenType::Eof)); // EOFを追加
+        Parser::new(tokens_with_eof)
+    }
+
+    /// TypeCastのテスト
+    /// 1 as int -> string;
+    #[test]
+    fn parse_cast_from_literal() {
+        // 期待される戻り値
+        let expected = ExpressionNode::TypeCast(Box::new(TypeCastNode {
+            from_type: Type::Integer,
+            to_type: Type::String,
+            expression: Box::new(ExpressionNode::Literal(Box::new(LiteralNode {
+                value: LiteralValue::Integer(1),
+            }))),
+        }));
+
+        // テストしたい関数の入力
+        let tokens = vec![
+            Token::new(1, 1, IntegerLiteral(1)),
+            Token::new(1, 1, TokenType::As),
+            Token::new(1, 1, TokenType::IntType),
+            Token::new(1, 1, TokenType::TypeCastArrow),
+            Token::new(1, 1, TokenType::StringType),
+            Token::new(1, 1, TokenType::Semicolon),
+        ];
+        let mut parser: Parser = create_parser_with_tokens(tokens);
+
+        // テストしたい関数の出力 (エラーが出ていないことを確認)
+        let result: Result<ExpressionNode, ParserError> = parse_type_cast(
+            &mut parser,
+            ExpressionNode::Literal(Box::new(LiteralNode {
+                value: LiteralValue::Integer(1),
+            })),
+        );
+        assert!(result.is_ok());
+
+        // 期待される戻り値と実際の戻り値が一致することを確認
+        assert_eq!(result.unwrap(), expected);
+    }
+}
