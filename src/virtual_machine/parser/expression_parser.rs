d34efe84e925fe1dd016a26a29f193f8e0480011
Shun <s.tsuchiya.business@gmail.com>
2024-11-24 13:01:55 +0900

    binary expressionに変数が入っていると落ちるバグを修正

--- src/virtual_machine/parser/expression_parser.rs
+++ src/virtual_machine/parser/expression_parser.rs
@@ -10,14 +10,11 @@ use crate::virtual_machine::parser::parser_error::ParserError;
 use crate::virtual_machine::parser::Parser;
 use crate::virtual_machine::token::token_type::TokenType;
 use parse_binary::parse_binary;
-use parse_call_of_variable_and_function::parse_identifier_or_call;
 use parse_parenthesized::parse_parenthesized;
 
 pub fn parse_expression(parser: &mut Parser) -> Result<ExpressionNode, ParserError> {
     let expr: ExpressionNode = if parser.check(TokenType::LeftParen) {
         parse_parenthesized(parser)?
-    } else if let TokenType::Identifier(_) = parser.peek().token_type {
-        parse_identifier_or_call(parser)?
     } else {
         parse_binary(parser)?
     };
