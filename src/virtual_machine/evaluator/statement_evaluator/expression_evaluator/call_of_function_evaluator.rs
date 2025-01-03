acb7942dec8cf71b376adb2b6f2a83227ebaa844
Shun <s.tsuchiya.business@gmail.com>
2025-01-03 22:38:08 +0900

    add test to validator

--- src/virtual_machine/evaluator/statement_evaluator/expression_evaluator/call_of_function_evaluator.rs
+++ src/virtual_machine/evaluator/statement_evaluator/expression_evaluator/call_of_function_evaluator.rs
@@ -77,6 +77,28 @@ fn generate_literal_node(value: LiteralValue) -> ExpressionNode {
     ExpressionNode::Literal(Box::new(LiteralNode { value }))
 }
 
+/// 関数呼び出しの引数の情報を元に、呼び出された関数の引数の情報を検証
+///
+/// - 引数の数が一致しているか
+/// - 引数の名前が一致しているか
+/// - 型が一致しているかを検証
+///
+/// ## Arguments
+///
+/// * `evaluator` - Evaluator
+/// * `calling_function_name` - 関数呼び出しの関数名
+/// * `calling_function_params` - 関数呼び出しの引数の情報
+/// * `called_function_params` - 呼び出された関数の引数の情報
+///
+/// ## Returns
+///
+/// * `Result<Vec<(String, Type, LiteralValue)>, EvaluationError>` - Ok(Vec<(String, Type, LiteralValue)>)
+///
+/// ## Raises
+///
+/// * `EvaluationError::ArgumentLengthError` - 引数の数が一致しない場合
+/// * `EvaluationError::ParameterTypeMismatch` - 引数の型が一致しない場合
+/// * `EvaluationError::ParameterNotFound` - 引数の名前が一致しない場合
 fn validate_params(
     evaluator: &mut Evaluator,
     calling_function_name: String,
@@ -99,12 +121,11 @@ fn validate_params(
     // 引数の型が一致しているかチェック
     let mut i: usize = 0;
     while i < calling_function_params.len() {
-        let calling_function_param_name: String = called_function_params[i].0.clone();
+        let calling_function_param_name: String = calling_function_params[i].0.clone();
         let calling_function_param_value: LiteralValue =
             evaluate_expression(evaluator, calling_function_params[i].1.clone())?;
         let calling_function_param_type: Type =
             literal_to_type(calling_function_param_value.clone());
-        let called_function_param_type: Type = called_function_params[i].1.clone();
 
         // indexを使った処理はここで終わるので、インクリメント
         i += 1;
@@ -113,22 +134,22 @@ fn validate_params(
             calling_function_param_name.clone(),
             called_function_params.clone(),
         ) {
-            Some(param_type) => {
+            Some(called_param_type) => {
                 // パラメータの型と一致していない
                 // let f: fn = (x: int): Void { return none; };
                 // f(x: 1.0); // ParameterTypeMismatch
-                if called_function_param_type.clone() != param_type.clone() {
+                if calling_function_param_type.clone() != called_param_type.clone() {
                     return Err(EvaluationError::ParameterTypeMismatch {
                         function_name: calling_function_name.clone(),
                         param_name: calling_function_param_name.clone(),
                         line: evaluator.line,
-                        expected: calling_function_param_type.to_string(),
-                        actual: param_type.to_string(),
+                        expected: called_param_type.to_string(),
+                        actual: calling_function_param_type.to_string(),
                     });
                 }
                 result.push((
                     calling_function_param_name,
-                    param_type,
+                    called_param_type,
                     calling_function_param_value,
                 ));
             }
@@ -147,6 +168,16 @@ fn validate_params(
     Ok(result)
 }
 
+/// 呼び出しに用いられた引数の名前が、呼び出された関数の引数の中に存在するかを検証
+///
+/// ## Arguments
+///
+/// * `name` - 引数の名前
+/// * `params` - 呼び出された関数の引数の情報
+///
+/// ## Returns
+///
+/// * `Option<Type>` - 呼び出された関数の引数の型
 fn search_argument_name_in_called_function_params(
     name: String,
     params: Vec<(String, Type)>,
@@ -172,11 +203,12 @@ fn literal_to_type(value: LiteralValue) -> Type {
 mod tests {
     use crate::virtual_machine::ast::LiteralNode;
     use crate::virtual_machine::ast::{ExpressionNode, LiteralValue, Type, VariableDeclarationNode, AST};
+    use crate::virtual_machine::evaluator::core::initialize_evaluator_with_custom_ast;
     use crate::virtual_machine::evaluator::evaluation_error::EvaluationError;
     use crate::virtual_machine::evaluator::Evaluator;
     use crate::virtual_machine::evaluator::mapper::function_mapper::FunctionMapper;
     use crate::virtual_machine::evaluator::mapper::variable_mapper::VariableMapper;
-    use crate::virtual_machine::evaluator::statement_evaluator::expression_evaluator::call_of_function_evaluator::setup_scope;
+    use crate::virtual_machine::evaluator::statement_evaluator::expression_evaluator::call_of_function_evaluator::{setup_scope, validate_params};
 
     /// setup_scope 関数は引数の情報を元にevaluatorを正しく初期化する
     ///
@@ -211,5 +243,193 @@ mod tests {
                 Err(_) => panic!("test_setup_scope failed: 作成した変数が見つかりません"),
             };
         assert_eq!(variable_data, expected);
+    }
+
+    /// validate_params 関数は正しい関数への入力を正常に検証する
+    ///
+    /// let f: fn = (x: int, y: float): int { return x; };
+    /// f(x: 1, y: 1.0); -- 正常な関数呼び出し
+    #[test]
+    fn test_validate_params() {
+        // 期待される値
+        let expected: Vec<(String, Type, LiteralValue)> = vec![
+            ("x".to_string(), Type::Integer, LiteralValue::Integer(1)),
+            ("y".to_string(), Type::Float, LiteralValue::Float(1.0)),
+        ];
+
+        // テスト対象のセットアップ
+        let mut evaluator: Evaluator = initialize_evaluator_with_custom_ast(vec![]);
+        let calling_function_name: String = "f".to_string();
+        let calling_function_params: Vec<(String, ExpressionNode)> = vec![
+            (
+                "x".to_string(),
+                ExpressionNode::Literal(Box::new(LiteralNode {
+                    value: LiteralValue::Integer(1),
+                })),
+            ),
+            (
+                "y".to_string(),
+                ExpressionNode::Literal(Box::new(LiteralNode {
+                    value: LiteralValue::Float(1.0),
+                })),
+            ),
+        ];
+        let called_function_params: Vec<(String, Type)> = vec![
+            ("x".to_string(), Type::Integer),
+            ("y".to_string(), Type::Float),
+        ];
+
+        // テスト対象の実行
+        let r: Result<Vec<(String, Type, LiteralValue)>, EvaluationError> = validate_params(
+            &mut evaluator,
+            calling_function_name.clone(),
+            calling_function_params.clone(),
+            called_function_params.clone(),
+        );
+
+        // 結果の検証
+        assert_eq!(r, Ok(expected));
+    }
+
+    /// validate_params 関数は引数の数が一致しない場合、エラーを返す
+    ///
+    /// let f: fn = (x: int, y: float): int { return x; };
+    /// f(x: 1); -- 引数の数が一致しない
+    #[test]
+    fn test_validate_params_argument_length_error() {
+        // テスト対象のセットアップ
+        let mut evaluator: Evaluator = initialize_evaluator_with_custom_ast(vec![]);
+        let calling_function_name: String = "f".to_string();
+        // -- let f: fn = (x: int, y: float): int { return x; }; の引数
+        let called_function_params: Vec<(String, Type)> = vec![
+            ("x".to_string(), Type::Integer),
+            ("y".to_string(), Type::Float),
+        ];
+        // -- let f: fn = (x: int, y: float): int { return x; }; の引数のうち、yがない
+        let calling_function_params: Vec<(String, ExpressionNode)> = vec![(
+            "x".to_string(),
+            ExpressionNode::Literal(Box::new(LiteralNode {
+                value: LiteralValue::Integer(1),
+            })),
+        )];
+
+        // テスト対象の実行
+        let r: Result<Vec<(String, Type, LiteralValue)>, EvaluationError> = validate_params(
+            &mut evaluator,
+            calling_function_name.clone(),
+            calling_function_params.clone(),
+            called_function_params.clone(),
+        );
+
+        // 結果の検証
+        assert_eq!(
+            r,
+            Err(EvaluationError::ArgumentLengthError {
+                function_name: calling_function_name.clone(),
+                expected: 2,
+                actual: 1,
+                line: 0
+            })
+        );
+    }
+
+    /// validate_params 関数は引数の名前が一致しない場合、エラーを返す
+    ///
+    /// let f: fn = (x: int, y: float): int { return x; };
+    /// f(z: 1, y: 1.0); -- 引数の名前が一致しない
+    #[test]
+    fn test_validate_params_parameter_not_found() {
+        // テスト対象のセットアップ
+        let mut evaluator: Evaluator = initialize_evaluator_with_custom_ast(vec![]);
+        let calling_function_name: String = "f".to_string();
+        // -- let f: fn = (x: int, y: float): int { return x; }; の引数
+        let called_function_params: Vec<(String, Type)> = vec![
+            ("x".to_string(), Type::Integer),
+            ("y".to_string(), Type::Float),
+        ];
+        // -- zは存在しない引数
+        let calling_function_params: Vec<(String, ExpressionNode)> = vec![
+            (
+                "z".to_string(),
+                ExpressionNode::Literal(Box::new(LiteralNode {
+                    value: LiteralValue::Integer(1),
+                })),
+            ),
+            (
+                "y".to_string(),
+                ExpressionNode::Literal(Box::new(LiteralNode {
+                    value: LiteralValue::Float(1.0),
+                })),
+            ),
+        ];
+
+        // テスト対象の実行
+        let r: Result<Vec<(String, Type, LiteralValue)>, EvaluationError> = validate_params(
+            &mut evaluator,
+            calling_function_name.clone(),
+            calling_function_params.clone(),
+            called_function_params.clone(),
+        );
+
+        // 結果の検証
+        assert_eq!(
+            r,
+            Err(EvaluationError::ParameterNotFound {
+                function_name: calling_function_name.clone(),
+                param_name: "z".to_string(),
+                line: 0
+            })
+        );
+    }
+
+    /// validate_params 関数は引数の型が一致しない場合、エラーを返す
+    ///
+    /// let f: fn = (x: int, y: float): int { return x; };
+    /// f(x: 1, y: 1); -- 引数yの型が一致しない
+    #[test]
+    fn test_validate_params_parameter_type_mismatch() {
+        // テスト対象のセットアップ
+        let mut evaluator: Evaluator = initialize_evaluator_with_custom_ast(vec![]);
+        let calling_function_name: String = "f".to_string();
+        // -- let f: fn = (x: int, y: float): int { return x; }; の引数
+        let called_function_params: Vec<(String, Type)> = vec![
+            ("x".to_string(), Type::Integer),
+            ("y".to_string(), Type::Float),
+        ];
+        // -- yの型が一致しない: Expected: float, Actual: int
+        let calling_function_params: Vec<(String, ExpressionNode)> = vec![
+            (
+                "x".to_string(),
+                ExpressionNode::Literal(Box::new(LiteralNode {
+                    value: LiteralValue::Integer(1),
+                })),
+            ),
+            (
+                "y".to_string(),
+                ExpressionNode::Literal(Box::new(LiteralNode {
+                    value: LiteralValue::Integer(1),
+                })),
+            ),
+        ];
+
+        // テスト対象の実行
+        let r: Result<Vec<(String, Type, LiteralValue)>, EvaluationError> = validate_params(
+            &mut evaluator,
+            calling_function_name.clone(),
+            calling_function_params.clone(),
+            called_function_params.clone(),
+        );
+
+        // 結果の検証
+        assert_eq!(
+            r,
+            Err(EvaluationError::ParameterTypeMismatch {
+                function_name: calling_function_name.clone(),
+                param_name: "y".to_string(),
+                line: 0,
+                expected: Type::Float.to_string(),
+                actual: Type::Integer.to_string()
+            })
+        );
     }
 }
